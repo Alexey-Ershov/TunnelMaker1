4fe99406eecaad8753df0100edf0787e7a59100c
Alexey-Ershov <ershov_ae@mail.ru>
2019-04-06 11:41:11 +0300

    Save route ID in the map: BDname -> routeID

--- TunnelMaker.cc
+++ TunnelMaker.cc
@@ -9,6 +9,8 @@
 #include <curlpp/Exception.hpp>
 #include <curlpp/Infos.hpp>
 
+/*#include <nlohmann/json.hpp>*/
+
 #include <boost/format.hpp>
 
 #include <cstdlib>
@@ -30,13 +32,15 @@ void TunnelMaker::init(Loader* loader, const Config& config)
             cli->print("{:-^90}", "  TUNNEL MAKER  ");
 
             try {
+                // Create Bridge Domain.
                 curlpp::Cleanup cleaner;
-                curlpp::Easy request;
+                curlpp::Easy put_request;
 
                 using namespace curlpp::Options;
+                using json = nlohmann::json;
                 
-                // request.setOpt(Verbose(true));
-                request.setOpt(
+                // put_request.setOpt(Verbose(true));
+                put_request.setOpt(
                         Url("http://172.30.7.201:8080/bridge_domains/"));
 
                 std::string put_request_data = (boost::format(
@@ -71,14 +75,51 @@ void TunnelMaker::init(Loader* loader, const Config& config)
                      % match[6]
                      % match[7]).str();
 
-                request.setOpt(new curlpp::options::PostFields(
+                put_request.setOpt(new curlpp::options::PostFields(
                         put_request_data));
-                request.setOpt(new curlpp::options::PostFieldSize(
+                put_request.setOpt(new curlpp::options::PostFieldSize(
                         put_request_data.length()));
 
-                request.setOpt(new curlpp::options::CustomRequest{"PUT"});
+                put_request.setOpt(new curlpp::options::CustomRequest{"PUT"});
 
-                request.perform();
+                std::ostringstream response;
+                put_request.setOpt(
+                        new curlpp::options::WriteStream(&response));
+
+                put_request.perform();
+
+                json json_response = json::parse(response.str());
+                
+                if (json_response["res"] == "ok") {
+                    cli->print("Tunnel between switches with dpid {} and {} "
+                               "was made successfully",
+                               match[2], match[5]);
+                }
+
+                // Get route id.
+                curlpp::Easy get_request;
+                get_request.setOpt(
+                        Url("http://172.30.7.201:8080/bridge_domains/"));
+
+                response.str(std::string());
+                response.clear();
+
+                get_request.setOpt(
+                        new curlpp::options::WriteStream(&response));
+
+                get_request.perform();
+
+                /*DLOG(INFO) << "response.str() =\n" << response.str();
+                DLOG(INFO) << "JSON dump =\n" << json_response.dump(4);*/
+
+                json_response = json::parse(response.str());
+                for (auto& item: json_response["array"]) {
+                    if (item["name"] == match[1]) {
+                        bd_routes_[item["name"]] =
+                                std::stoi(std::string((item["routesId"])[0]));
+                        break;
+                    }
+                }
 
             } catch (curlpp::LogicError& e) {
                 LOG(ERROR) << e.what();
@@ -89,7 +130,7 @@ void TunnelMaker::init(Loader* loader, const Config& config)
 
             /*try {
                 curlpp::Cleanup cleaner;
-                curlpp::Easy request;
+                curlpp::Easy put_request;
                 
                 std::string url =
                         std::string("http://172.30.7.201:8080/routes/id/") +
@@ -97,19 +138,19 @@ void TunnelMaker::init(Loader* loader, const Config& config)
                         std::string("/add-path/");
 
                 using namespace curlpp::Options;
-                // request.setOpt(Verbose(true));
-                request.setOpt(Url(url));
+                // put_request.setOpt(Verbose(true));
+                put_request.setOpt(Url(url));
 
                 std::string post_data = "{\"broken_flag\": \"true\"}";
 
                 DLOG(INFO) << "DEBUG: " << post_data.length();
                 
-                request.setOpt(new curlpp::options::PostFields(
+                put_request.setOpt(new curlpp::options::PostFields(
                         post_data));
-                request.setOpt(new curlpp::options::PostFieldSize(
+                put_request.setOpt(new curlpp::options::PostFieldSize(
                         post_data.length()));
 
-                request.perform();
+                put_request.perform();
 
             } catch (curlpp::LogicError& e) {
                 LOG(ERROR) << e.what();
@@ -118,8 +159,6 @@ void TunnelMaker::init(Loader* loader, const Config& config)
                 LOG(ERROR) << e.what();
             }*/
 
-            /*cli->print("Tunnel between switches with dpid {} and {} was made",
-                       match[1], match[2]);*/
             cli->print("{:-^90}", "");
         }
     );
--- TunnelMaker.hpp
+++ TunnelMaker.hpp
@@ -3,6 +3,9 @@
 #include "Application.hpp"
 #include "Loader.hpp"
 
+#include <string>
+#include <map>
+
 namespace runos { 
 
 class TunnelMaker : public Application {
@@ -10,6 +13,9 @@ class TunnelMaker : public Application {
     SIMPLE_APPLICATION(TunnelMaker, "tunnel-maker")
 public:
     void init(Loader* loader, const Config& config) override;
+
+private:
+    std::map<std::string, int> bd_routes_;
 };
 
 } // namespace runos
