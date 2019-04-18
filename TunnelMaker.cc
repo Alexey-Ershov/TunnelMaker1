86273108dd68092dbc4f6dd5c17ca01975ecca33
Alexey-Ershov <ershov_ae@mail.ru>
2019-04-18 18:08:38 +0300

    Add paths for every client

--- TunnelMaker.cc
+++ TunnelMaker.cc
@@ -1,5 +1,4 @@
 #include "TunnelMaker.hpp"
-#include "Topology.hpp"
 #include <runos/core/logging.hpp>
 
 #include <curlpp/cURLpp.hpp>
@@ -23,7 +22,8 @@ REGISTER_APPLICATION(TunnelMaker, {"command-line", ""})
 void TunnelMaker::init(Loader* loader, const Config& config)
 {
     cli_ = CommandLine::get(loader);
-    Topology* topo = Topology::get(loader);
+    topo_ = Topology::get(loader);
+    ip_ = config_get(config_cd(config, "tunnel-maker"), "ip", "127.0.0.1");
 
     cli_->register_command(
         cli_pattern(R"(mktun\s+([-\w]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+--hops\s+([0-9]+))"),
@@ -31,15 +31,23 @@ void TunnelMaker::init(Loader* loader, const Config& config)
         {
             cli_->print("{:-^90}", "  TUNNEL MAKER  ");
 
+            match_ = match;
+
             try {
-                create_bd(match);
+                create_bd();
 
-                fetch_route_id();
+                if (was_created_) {
+                    fetch_route_id();
 
-                /*DLOG(INFO) << "AFTER ADDING NEW BD";
-                for (auto& it: bd_routes_) {
-                    DLOG(INFO) << it.first << ": " << it.second;
-                }*/
+                    /*DLOG(INFO) << "AFTER ADDING NEW BD";
+                    for (auto& it: bd_routes_) {
+                        DLOG(INFO) << it.first << ": " << it.second;
+                    }*/
+
+                    check_tunnel_requirements();
+
+                    while(add_path(match[1]));
+                }
 
             } catch (curlpp::LogicError& e) {
                 LOG(ERROR) << e.what();
@@ -47,56 +55,27 @@ void TunnelMaker::init(Loader* loader, const Config& config)
             } catch (curlpp::RuntimeError& e) {
                 LOG(ERROR) << e.what();
             }
+            
+            cli_->print("{:-^90}", "");
+        }
+    );
 
-            auto path = topo->getFirstWorkPath(bd_routes_[match[1]]);
-            /*DLOG(INFO) << "DEBUG: path.size = " << path.size();*/
-            if (static_cast<int>((path.size() - 2) / 2) >
-                    std::stoi(match[8]) - 2) {
-                
-                LOG(ERROR) << "Can't create tunnel with such "
-                              "requirements";
+    cli_->register_command(
+        cli_pattern(R"(deltun\s+([-\w]+))"),
+        [=](cli_match const& match)
+        {
+            cli_->print("{:-^90}", "  TUNNEL MAKER  ");
+
+            try {
                 delete_bd(match[1]);
-            
-            } else if (was_created_) {
-                LOG(INFO) << "Tunnel between switches with dpid "
-                          << match[2]
-                          << " and "
-                          << match[5]
-                          << " was made successfully";
-            }
-
-            
-            /*try {
-                curlpp::Cleanup cleaner;
-                curlpp::Easy request;
-                
-                std::string url =
-                        std::string("http://172.30.7.201:8080/routes/id/") +
-                        std::string(match[1]) +
-                        std::string("/add-path/");
-
-                using namespace curlpp::Options;
-                // request.setOpt(Verbose(true));
-                request.setOpt(Url(url));
-
-                std::string post_data = "{\"broken_flag\": \"true\"}";
-
-                DLOG(INFO) << "DEBUG: " << post_data.length();
-                
-                request.setOpt(new curlpp::options::PostFields(
-                        post_data));
-                request.setOpt(new curlpp::options::PostFieldSize(
-                        post_data.length()));
-
-                request.perform();
 
             } catch (curlpp::LogicError& e) {
                 LOG(ERROR) << e.what();
             
             } catch (curlpp::RuntimeError& e) {
                 LOG(ERROR) << e.what();
-            }*/
-
+            }
+            
             cli_->print("{:-^90}", "");
         }
     );
@@ -116,12 +95,14 @@ void TunnelMaker::startUp(Loader* loader)
 }
 
 
-void TunnelMaker::create_bd(cli_match const& match)
+void TunnelMaker::create_bd()
 {
     curlpp::Easy request;
 
     request.setOpt(
-            Url("http://172.30.7.201:8080/bridge_domains/"));
+            Url(std::string("http://") +
+                ip_ +
+                std::string(":8080/bridge_domains/")));
 
     std::string request_data = (boost::format(
     "{"
@@ -147,13 +128,13 @@ void TunnelMaker::create_bd(cli_match const& match)
             "}"
         "],"
         "\"type\": \"P2P\""
-    "}") % match[1]
-         % match[2]
-         % match[3]
-         % match[4]
-         % match[5]
-         % match[6]
-         % match[7]).str();
+    "}") % match_[1]
+         % match_[2]
+         % match_[3]
+         % match_[4]
+         % match_[5]
+         % match_[6]
+         % match_[7]).str();
 
     request.setOpt(new curlpp::options::PostFields(request_data));
     request.setOpt(new curlpp::options::PostFieldSize(request_data.length()));
@@ -183,7 +164,9 @@ void TunnelMaker::fetch_route_id(std::string name)
 
     curlpp::Easy request;
     request.setOpt(
-            Url("http://172.30.7.201:8080/bridge_domains/"));
+            Url(std::string("http://") +
+                ip_ +
+                std::string(":8080/bridge_domains/")));
 
     std::ostringstream response;
     response.str(std::string());
@@ -214,8 +197,110 @@ void TunnelMaker::delete_bd(std::string name)
 {
     curlpp::Easy request;
     
-    std::string url = std::string("http://172.30.7.201:8080/bridge_domains/") +
+    std::string url = std::string("http://") +
+                      ip_ +
+                      std::string(":8080/bridge_domains/") +
                       name +
+                      std::string("/");
+    
+    request.setOpt(Url(url));
+    request.setOpt(new curlpp::options::CustomRequest{"DELETE"});
+    
+    std::ostringstream response;
+    response.str(std::string());
+    response.clear();
+    request.setOpt(new curlpp::options::WriteStream(&response));
+    
+    request.perform();
+}
+
+void TunnelMaker::check_tunnel_requirements()
+{
+    auto path = topo_->getFirstWorkPath(bd_routes_[match_[1]]);
+    /*DLOG(INFO) << "DEBUG: path.size = " << path.size();*/
+    if (static_cast<int>((path.size() - 2) / 2) >
+            std::stoi(match_[8]) - 2) {
+        
+        LOG(ERROR) << "Can't create tunnel with such "
+                      "requirements";
+        delete_bd(match_[1]);
+
+    } else if (was_created_) {
+        LOG(INFO) << "Tunnel between switches with dpid "
+                  << match_[2]
+                  << " and "
+                  << match_[5]
+                  << " was made successfully";
+    }
+}
+
+bool TunnelMaker::add_path(std::string name)
+{
+    curlpp::Easy request;
+    
+    std::string url =
+            std::string("http://") +
+            ip_ +
+            std::string(":8080/routes/id/") +
+            std::to_string(bd_routes_[name]) +
+            std::string("/add-path/");
+
+    using namespace curlpp::Options;
+    request.setOpt(Url(url));
+
+    std::string post_data = "{\"broken_flag\": \"true\"}";
+
+    request.setOpt(new curlpp::options::PostFields(
+            post_data));
+    request.setOpt(new curlpp::options::PostFieldSize(
+            post_data.length()));
+
+    std::ostringstream response;
+    response.str(std::string());
+    response.clear();
+    request.setOpt(new curlpp::options::WriteStream(&response));
+
+    request.perform();
+
+    // DLOG(INFO) << "### RESPONSE ###\n" << response.str();
+
+    json json_response = json::parse(response.str());
+    if (json_response["act"] == "path created") {
+        auto route_id = std::stoi(std::string(json_response["route_id"]));
+        auto path_id = std::stoi(std::string(json_response["path_id"]));
+
+        auto path = topo_->getPath(route_id, path_id);
+        DLOG(INFO) << "### PATH ###";
+        for (auto& it: path) {
+            DLOG(INFO) << it.dpid << ":" << it.port;
+        }
+
+        if (static_cast<int>((path.size() - 2) / 2) >
+                std::stoi(match_[8]) - 2) {
+            
+            // LOG(ERROR) << "Can't add path";
+            delete_path(std::to_string(route_id),
+                        std::to_string(path_id));
+
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    return true;
+}
+
+void TunnelMaker::delete_path(std::string route_id, std::string path_id)
+{
+    curlpp::Easy request;
+
+    std::string url = std::string("http://") +
+                      ip_ +
+                      std::string(":8080/routes/id/") +
+                      route_id +
+                      std::string("/delete-path/") +
+                      path_id +
                       std::string("/");
     
     request.setOpt(Url(url));
--- TunnelMaker.hpp
+++ TunnelMaker.hpp
@@ -3,6 +3,7 @@
 #include "Application.hpp"
 #include "Loader.hpp"
 #include "CommandLine.hpp"
+#include "Topology.hpp"
 
 #include <string>
 #include <map>
@@ -18,12 +19,18 @@ public:
 
 private:
     std::map<std::string, int> bd_routes_;
+    Topology* topo_;
     CommandLine* cli_;
-    bool was_created_;
+    cli_match match_;
+    bool was_created_ = false;
+    std::string ip_;
 
-    void create_bd(cli_match const& match);
+    void create_bd();
     void fetch_route_id(std::string name = "");
     void delete_bd(std::string name);
+    void check_tunnel_requirements();
+    bool add_path(std::string name);
+    void delete_path(std::string route_id, std::string path_id);
 };
 
 } // namespace runos
